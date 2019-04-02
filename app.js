f4242f191a2dada9a737210abd51fde01a42db1a
RetroAstro <452461114@qq.com>
2019-04-03 00:54:50 +0800

    edit

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const { setFreshJWT } = require('./api/index')
 
 App({
    onLaunch () {
-      var { exp } = qq.getStorageSync('jwt') || {}
-
-      if (!exp) qq.redirectTo({ url: '/pages/auth/auth' })
-      
       var time_stamp = new Date().getTime()
-      if (exp && time_stamp > exp) setFreshJWT()
+      var { exp } = qq.getStorageSync('jwt') || {}
+      
+      if (!exp) {
+         qq.redirectTo({ url: '/pages/auth/auth' })
+      } else if (time_stamp > exp) {
+         setFreshJWT()
+      }
    }
 })
