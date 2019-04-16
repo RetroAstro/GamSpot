f8e03feda65247bc6a39899e5234a90e7611de57
RetroAstro <452461114@qq.com>
2019-04-16 21:17:11 +0800

    state

--- api/urls.js
+++ api/urls.js
@@ -1,10 +1,12 @@
 const { baseUrl } = require('./config')
 
-const url = route => baseUrl + route
-
-module.exports = {
-   GET_FRESH_JWT: url('/app/login'),
-   SEND_BIND_DATA: url('/app/binding'),
-   SELECT_GENDER: url('/app/selectGender'),
-   RESET_ALL_DATA: url('/refreshDB')
+const routes = {
+   GET_FRESH_JWT: '/app/login',
+   SEND_BIND_DATA: '/app/binding',
+   SELECT_GENDER: '/app/selectGender',
+   RESET_ALL_DATA: '/refreshDB'
 }
+
+const urls = Object.entries(routes).map(([key, value]) => ({ [key]: baseUrl + value })).reduce((prev, next) => ({ ...prev, ...next }))
+
+module.exports = urls
