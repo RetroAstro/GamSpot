e764ae4bd9b5b6aa42a0d40df436a4ca73f9d99a
RetroAstro <452461114@qq.com>
2019-05-08 12:49:26 +0800

    format

--- api/alter.js
+++ api/alter.js
@@ -26,8 +26,8 @@ const normalizeCommit = commit => {
             if (commit.length) recursive(commit, nickname)
          })
       }
-
       recursive(commit, recipient)
+      
       return result
    }
 
